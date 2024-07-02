f5ba465081fee49cb065c1240c8c5469f3d386f0
Jona Subba <sanarung.subba@gmail.com>
2024-07-02 11:43:35 +0545

    pagination update

--- src/components/shop/Pagination.jsx
+++ src/components/shop/Pagination.jsx
@@ -1,52 +1,14 @@
+'use client';
 
 import useStore from "@/zustand/slice";
 import Product from "../home/products/Product";
 import { makeSelectedBrands, makeSelectedCategories, makeSelectedColors } from "@/zustand/selectors";
 import { paginationItems } from "@/Constants";
-import Paginate from "./Paginate";
+import { useState } from "react";
 
-const items = paginationItems;  
+const items = paginationItems;
 
-/* function Items({ currentItems, selectedBrands, selectedCategories, selectedColors }) {
-  // Filter items based on selected brands and categories
-  const filteredItems = currentItems.filter((item) => {
-    const isBrandSelected =
-      selectedBrands.length === 0 ||
-      selectedBrands.some((brand) => brand.title === item.brand);
-
-    const isCategorySelected =
-      selectedCategories.length === 0 ||
-      selectedCategories.some((category) => category.title === item.cat);
-
-    const isColorSelected =
-      selectedColors.length === 0 ||
-      selectedColors.some((color) => color.title === item.color);
-
-    return isBrandSelected && isCategorySelected && isColorSelected;
-  });
-
-  
-
-  return (
-    <>
-      {entries.map((item) => (
-        <div key={item._id} className="w-full">
-          <Product
-            _id={item._id}
-            img={item.img}
-            productName={item.productName}
-            price={item.price}
-            color={item.color}
-            des={item.des}
-          />
-        </div>
-      ))}   
-      
-    </>
-  );
-} */
-
-const Pagination = ({ currentItems, searchParams }) => {
+const Pagination = ({ currentItems }) => {
   const selectedBrands = useStore(makeSelectedBrands);
   const selectedCategories = useStore(makeSelectedCategories);
   const selectedColors = useStore(makeSelectedColors);
@@ -66,36 +28,49 @@ const Pagination = ({ currentItems, searchParams }) => {
 
     return isBrandSelected && isCategorySelected && isColorSelected;
   });
-  const page = searchParams?.['page'] ?? '1'
-  const per_page = searchParams?.['per_page'] ?? '5'
 
-  // mocked, skipped and limited in the real app
-  const start = (Number(page) - 1) * Number(per_page) // 0, 5, 10 ...
-  const end = start + Number(per_page) // 5, 10, 15 ...
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5; // Number of items per page
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  
+ 
+  const entries = filteredItems.slice(startIndex, endIndex);
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+};
 
-  const entries = filteredItems.slice(start, end);
-
+const handlePrevPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+};
 
   return (
     <>
-    <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 mdl:gap-4 lg:gap-10'>
-      {entries.map((item) => (
-        <div key={item._id} className="w-full">
-          <Product
-            _id={item._id}
-            img={item.img}
-            productName={item.productName}
-            price={item.price}
-            color={item.color}
-            des={item.des}
-          />
-        </div>
-      ))}   
-    </div>
-    <Paginate
-        hasNextPage={end < items.length}
-        hasPrevPage={start > 0}
-      />
+      <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 mdl:gap-4 lg:gap-10'>
+        {entries.map((item) => (
+          <div key={item._id} className="w-full">
+            <Product
+              _id={item._id}
+              img={item.img}
+              productName={item.productName}
+              price={item.price}
+              color={item.color}
+              des={item.des}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="mt-8 flex justify-between w-full">
+        {currentPage > 1 && (
+          <button onClick={handlePrevPage} className="flex items-center justify-center px-3 h-8 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+            Prev
+          </button>
+        )}
+        {endIndex < currentItems.length && (
+          <button onClick={handleNextPage} className="flex items-center justify-center px-3 h-8 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</button>
+        )}
+      </div>
+
     </>
   )
 }
--- src/components/shop/shopBy/Brand.jsx
+++ src/components/shop/shopBy/Brand.jsx
@@ -3,54 +3,54 @@ import { useState } from "react";
 import NavTitle from "./NavTitle";
 import useStore from "@/zustand/slice";
 const Brand = () => {
-    const brands = [
-        {
-            _id: 900,
-            title: "Gucci",
-          },
-          {
-            _id: 901,
-            title: "Prada",
-          },
-          {
-            _id: 902,
-            title: "Goldstar",
-          },
-      
-          {
-            _id: 903,
-            title: "Zara",
-          },
-    ]
+  const brands = [
+    {
+      _id: 900,
+      title: "Gucci",
+    },
+    {
+      _id: 901,
+      title: "Prada",
+    },
+    {
+      _id: 902,
+      title: "Goldstar",
+    },
 
-    const [showBrands, setShowBrands] = useState(true);
-    const checkedBrands = useStore((state) => state.checkedBrands); // Use Zustand state
-    const toggleBrand = useStore((state) => state.toggleBrand);
+    {
+      _id: 903,
+      title: "Zara",
+    },
+  ]
+
+  const [showBrands, setShowBrands] = useState(true);
+  const checkedBrands = useStore((state) => state.checkedBrands); // Use Zustand state
+  const toggleBrand = useStore((state) => state.toggleBrand);
   return (
     <div>
-        <div onClick={() => setShowBrands(!showBrands)}>
-            <NavTitle title="Shop by Brand" icons={true} />
-        </div>
-        <div>
-          {showBrands && (
-            <ul className="flex flex-col gap-4 text-sm lg:text-base text-[#767676]">
-                {brands.map((item) => (
-                    <li
-                        key={item._id}
-                        className="border-b-[1px] border-b-[#F0F0F0] pb-2 flex items-center gap-2 hover:text-primeColor hover:border-gray-400 duration-300 cursor-pointer"
-                    >
-                      <input
+      <div className="cursor-pointer" onClick={() => setShowBrands(!showBrands)}>
+        <NavTitle title="Shop by Brand" icons={true} />
+      </div>
+      <div>
+        {showBrands && (
+          <ul className="flex flex-col gap-4 text-sm lg:text-base text-[#767676]">
+            {brands.map((item) => (
+              <li
+                key={item._id}
+                className="border-b-[1px] border-b-[#F0F0F0] pb-2 flex items-center gap-2 hover:text-primeColor hover:border-gray-400 duration-300 cursor-pointer"
+              >
+                <input
                   type="checkbox"
                   id={item._id}
                   checked={checkedBrands.some((b) => b._id === item._id)}
                   onChange={() => toggleBrand(item)} // Call the Zustand action
                 />
-                        {item.title}
-                    </li>
-                ))}
-            </ul>
-          )}
-        </div>
+                {item.title}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   )
 }
